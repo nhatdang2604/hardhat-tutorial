27a08b7e88edaaaa95604da9f2c2dc8cb5290c9d
dangkl123 <nhatdang2604@gmail.com>
2022-10-17 17:29:32 +0700

    Add test for transfering token between different account, in test/Token.js

--- test/Token.js
+++ test/Token.js
@@ -1,14 +1,36 @@
 const { expect } = require("chai");
 
 describe("Token contract", function () {
-  it("Deployment should assign the total supply of tokens to the owner", async function () {
-    const [owner] = await ethers.getSigners();
+    
+    it("Deployment should assign the total supply of tokens to the owner", async function () {
+        
+        const [owner] = await ethers.getSigners();
+        const Token = await ethers.getContractFactory("Token");
+        const hardhatToken = await Token.deploy();
 
-    const Token = await ethers.getContractFactory("Token");
+        //Compare if the total amount of token is equals with the balance of the 
+        //  person who deploying the contract
+        const ownerBalance = await hardhatToken.balanceOf(owner.address);
+        expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+            
+    });
 
-    const hardhatToken = await Token.deploy();
+    it("Should transfer token between accounts", async function() {
+        
+        const [owner, addr1, addr2] = await ethers.getSigners();
+        const Token = await ethers.getContractFactory("Token");
+        const hardhatToken = await Token.deploy();
 
-    const ownerBalance = await hardhatToken.balanceOf(owner.address);
-    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
-  });
+        //Transfer 50 tokens from owner to addr1
+        let amountOfTokenTransfered = 50;
+        await hardhatToken.transfer(addr1.address, amountOfTokenTransfered);
+        //Assert if the balance of addr1 is 50
+        expect(await hardhatToken.balanceOf(addr1.address)).to.equal(amountOfTokenTransfered);
+        
+        //Transfer 50 tokens from addr1 to addr2
+        await hardhatToken.connect(addr1).transfer(addr2.address, amountOfTokenTransfered);
+        //Assert if the balance of addr1 is 0 and addr2 is 50
+        expect(await hardhatToken.balanceOf(addr1.address)).to.equal(0);
+        expect(await hardhatToken.balanceOf(addr2.address)).to.equal(amountOfTokenTransfered);
+    }); 
 });
